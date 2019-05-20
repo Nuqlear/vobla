5c2037277622105fd3ca9ae0180eca763d1cb75c
Oleg Shigorin <mail@olegshigor.in>
2019-05-20 23:18:24 +0300

    Attempt to fix tests at drone

--- backend/manage.py
+++ backend/manage.py
@@ -17,27 +17,24 @@ from vobla.db.engine import create_engine
 from vobla.db.models import UserInvite
 
 
-TESTS_FOLDER = 'tests'
+TESTS_FOLDER = "tests"
 
 
 def run_server():
     tornado.platform.asyncio.AsyncIOMainLoop().install()
     app = TornadoApplication()
-    app.listen(config['vobla']['port'])
+    app.listen(config["vobla"]["port"])
     loop = tornado.ioloop.IOLoop.current()
     loop.start()
 
 
 def run_migrations():
-    alembic_args = [
-        '--raiseerr',
-        'upgrade', 'head',
-    ]
+    alembic_args = ["--raiseerr", "upgrade", "head"]
     alembic.config.main(argv=alembic_args)
 
 
 def run_tests():
-    sys.exit(pytest.main(['-vv', TESTS_FOLDER]))
+    sys.exit(pytest.main(["-vv", "-s", TESTS_FOLDER]))
 
 
 def create_invite():
@@ -54,27 +51,25 @@ def create_invite():
 
 
 def run_worker():
-    worker.worker(app=celery_app).run(**config['celery'])
+    worker.worker(app=celery_app).run(**config["celery"])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--migrate", help="run migrations", action="store_true")
     parser.add_argument(
-        "-m", "--migrate", help="run migrations", action="store_true"
-    )
-    parser.add_argument(
-        "command", type=str, choices=[
-            'migrate', 'runserver', 'runworker', 'runtests', 'createinv'
-        ]
+        "command",
+        type=str,
+        choices=["migrate", "runserver", "runworker", "runtests", "createinv"],
     )
     args = parser.parse_args()
-    if args.command == 'migrate' or args.migrate:
+    if args.command == "migrate" or args.migrate:
         run_migrations()
-    if args.command == 'runserver':
+    if args.command == "runserver":
         run_server()
-    elif args.command == 'createinv':
+    elif args.command == "createinv":
         create_invite()
-    elif args.command == 'runtests':
+    elif args.command == "runtests":
         run_tests()
-    elif args.command == 'runworker':
+    elif args.command == "runworker":
         run_worker()
